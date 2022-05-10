04b866bc9ea852e648879e13072c085f316680c0
Diana de Oliveira Martins <99148060+ddianaom@users.noreply.github.com>
2022-05-10 15:29:13 -0400

    Aula 6

--- Pixel Game/Assets/Scripts/Player.cs
+++ Pixel Game/Assets/Scripts/Player.cs
@@ -9,11 +9,13 @@ public class Player : MonoBehaviour
     public bool isJumping;
     public bool doubleJump;
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,8 +27,23 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+           if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
     void Jump()
@@ -37,6 +54,7 @@ public class Player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             }
             else
             {
@@ -56,6 +74,7 @@ public class Player : MonoBehaviour
         if(collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
 
